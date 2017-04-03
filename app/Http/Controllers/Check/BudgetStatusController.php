6b9c712086ced7f84e538c6615be565588d93c44
Marcel Thiesies <thiesies@bestit-online.de>
2017-04-03 20:25:25 +0200

    remove unneeded Api Controllers, added dummy check controllers for upcoming features

---
+++ app/Http/Controllers/Check/BudgetStatusController.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Check;
+
+use App\Http\Controllers\Controller;
+
+/**
+ * Class BudgetStatusController
+ * @package App\Http\Controllers\Check
+ */
+class BudgetStatusController extends Controller
+{
+    // TODO
+}
---
+++ app/Http/Controllers/Check/MissingTimeEntriesController.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Check;
+
+use App\Http\Controllers\Controller;
+
+/**
+ * Class MissingTimeEntriesController
+ * @package App\Http\Controllers\Check
+ */
+class MissingTimeEntriesController extends Controller
+{
+    // TODO
+}
